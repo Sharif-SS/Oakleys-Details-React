e015880610b1d9534f2452db7b4b2f9796d2599b
Sharif <78062547+Sharif-SS@users.noreply.github.com>
2024-04-27 16:17:17 -0230

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Elite-welding-and-fabrication-React
+ Website for a friend using React and Tailwind
+ 
+ https://elitewelding-nl.netlify.app/
---
+++ src/pages/home.jsx
@@ -0,0 +1,238 @@
+import React from "react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Typography,
+  Button,
+  IconButton,
+  Input,
+  Textarea,
+  Checkbox,
+} from "@material-tailwind/react";
+import { FingerPrintIcon, UsersIcon } from "@heroicons/react/24/solid";
+import { PageTitle, Footer } from "@/widgets/layout";
+import { FeatureCard, TeamCard } from "@/widgets/cards";
+import { featuresData, teamData, contactData } from "@/data";
+
+export function Home() {
+  return (
+    <>
+      <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
+        <div className="absolute top-0 h-full w-full bg-[url('/img/background-3.png')] bg-cover bg-center" />
+        <div className="absolute top-0 h-full w-full bg-black/60 bg-cover bg-center" />
+        <div className="max-w-8xl container relative mx-auto">
+          <div className="flex flex-wrap items-center">
+            <div className="ml-auto mr-auto w-full px-4 text-center lg:w-8/12">
+              <Typography
+                variant="h1"
+                color="white"
+                className="mb-6 font-black"
+              >
+                Hot Work, Cool Results.
+
+              </Typography>
+              <Typography variant="lead" color="white" className="opacity-80">
+              We specialize in MIG/TIG welding, custom fabrication, and the meticulous repair of aluminum, stainless steel, 
+              and rust-damaged parts.  
+              Our commitment to excellence ensures your project is completed to the highest standards.  
+              Need it done on-site? We'll bring the workshop to you!  Call now for your free quote.
+              </Typography>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section className="-mt-32 bg-white px-4 pb-20 pt-4">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+            {featuresData.map(({ color, title, icon, description }) => (
+              <FeatureCard
+                key={title}
+                color={color}
+                title={title}
+                icon={React.createElement(icon, {
+                  className: "w-5 h-5 text-white",
+                })}
+                description={description}
+              />
+            ))}
+          </div>
+          <div className="mt-32 flex flex-wrap items-center">
+            <div className="mx-auto -mt-8 w-full px-4 md:w-5/12">
+              <div className="mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-blue-gray-900 p-2 text-center shadow-lg">
+                <FingerPrintIcon className="h-8 w-8 text-white " />
+              </div>
+              <Typography
+                variant="h3"
+                className="mb-3 font-bold"
+                color="blue-gray"
+              >
+                Don't Just Take Our Word For It – See What Our Happy Customers Say
+              </Typography>
+              <Typography className="mb-8 font-normal text-blue-gray-500">
+                We're passionate about delivering the highest quality welding results.  But words can only go so far.
+                That's why we invite you to hear directly from the folks we've had the pleasure of serving.
+              </Typography>
+              
+              <a
+                href="https://www.facebook.com/profile.php?id=61556028688734&sk=reviews"
+                target="_blank"
+              >
+                <Button variant="filled">Read Reviews</Button>
+              </a>
+            </div>
+            <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
+              <Card className="shadow-lg border shadow-gray-500/10 rounded-lg">
+                <CardHeader floated={false} className="relative h-56">
+                  <img
+                    alt="Card Image"
+                    src="/img/teamwork.png"
+                    className="h-full w-full"
+                  />
+                </CardHeader>
+                <CardBody>
+                  <Typography variant="small" color="blue-gray" className="font-normal">Person X</Typography>
+                  <Typography
+                    variant="h5"
+                    color="blue-gray"
+                    className="mb-3 mt-2 font-bold"
+                  >
+                    Review Header Example X
+                  </Typography>
+                  <Typography className="font-normal text-blue-gray-500">
+                    Loren Ipsum
+                  </Typography>
+                </CardBody>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="px-4 pt-20 pb-48">
+        <div className="container mx-auto">
+          <PageTitle section="Our Team" heading="Here are our heroes">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed tortor vel mi 
+          sodales feugiat id non leo. Integer pulvinar lacus quis iaculis consectetur
+          </PageTitle>
+          <div className="mt-24 grid grid-cols-1 gap-12 gap-x-24 md:grid-cols-2 xl:grid-cols-2">
+            {teamData.map(({ img, name, position, socials }) => (
+              <TeamCard
+                key={name}
+                img={img}
+                name={name}
+                position={position}
+                socials={
+                  <div className="flex items-center gap-2">
+                    {socials.map(({ color, name }) => (
+                      <IconButton key={name} color={color} variant="text">
+                        <i className={`fa-brands text-xl fa-${name}`} />
+                      </IconButton>
+                    ))}
+                  </div>
+                }
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className="relative bg-white py-24 px-4">
+        <div className="container mx-auto">
+          <PageTitle section="Co-Working" heading="Build something">
+          unc iaculis suscipit enim sed feugiat. Integer tincidunt felis quam, vel mattis est dignissim vitae. 
+          Duis porta quis ligula at ultrices. Fusce laoreet lorem sit amet mi faucibus congue.
+          </PageTitle>
+          <div className="mx-auto mt-20 mb-48 grid max-w-5xl grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3">
+            {contactData.map(({ title, icon, description }) => (
+              <Card
+                key={title}
+                color="transparent"
+                shadow={false}
+                className="text-center text-blue-gray-900"
+              >
+                <div className="mx-auto mb-6 grid h-14 w-14 place-items-center rounded-full bg-blue-gray-900 shadow-lg shadow-gray-500/20">
+                  {React.createElement(icon, {
+                    className: "w-5 h-5 text-white",
+                  })}
+                </div>
+                <Typography variant="h5" color="blue-gray" className="mb-2">
+                  {title}
+                </Typography>
+                <Typography className="font-normal text-blue-gray-500">
+                  {description}
+                </Typography>
+              </Card>
+            ))}
+          </div>
+          <PageTitle section="Contact Us" heading="Want to work with us?">
+            Complete this form and we will get back to you in 24 hours.
+          </PageTitle>
+
+
+          {/* <!-- dummy so netlify knows what's coming -->
+                    <div class="container">
+                      <h1 class="title">
+                        Contact
+                      </h1>
+                      <div class="content">
+                        
+                    <form name="contact" action="/thank-you" netlify-honeypot="bot-field" method="post" netlify>
+                          <input type="hidden" name="form-name" value="contact" />
+                          <p class="hidden">     
+                            <label>Don’t fill this out: <input name="bot-field"></label>   
+                          </p>
+                          <label class="form-label" for="name">
+                            Name:
+                          </label>
+                          <input class="form-field" name="name" id="name" />
+                          <label class="form-label" for="email">
+                            Email:
+                          </label>
+                          <input class="form-field" name="email" id="email" />
+                          <label class="form-label" for="message">
+                            Message:
+                          </label>
+                          <textarea class="form-field" name="message" id="message"></textarea>
+                          <input class="form-button" type="submit" value="Send message" />
+                        </form>
+                      </div>
+                    </div>  */}
+
+          
+          <form name="contact" className="mx-auto w-full mt-12 lg:w-5/12" netlify>
+            <div className="mb-8 flex gap-8">
+              <Input variant="outlined" size="lg" label="Full Name" />
+              <Input variant="outlined" size="lg" label="Email Address" />
+            </div>
+            <Textarea variant="outlined" size="lg" label="Message" rows={8} />
+            <Checkbox
+              label={
+                <Typography
+                  variant="small"
+                  color="gray"
+                  className="flex items-center font-normal"
+                >
+                  I agree the
+                  <a
+                    href="#"
+                    className="font-medium transition-colors hover:text-gray-900"
+                  >
+                    &nbsp;Terms and Conditions
+                  </a>
+                </Typography>
+              }
+              containerProps={{ className: "-ml-2.5" }}
+            />
+            <Button variant="gradient" size="lg" className="mt-8" fullWidth type="submit">
+              Send Message
+            </Button>
+          </form>
+        </div>
+      </section>
+      <div className="bg-white">
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default Home;
